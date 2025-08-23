4e7855f5227d3f310e21308970ccc9bbbe20daf7
d0d0d0d0k <kdoeun05@konkuk.ac.kr>
2025-08-23 16:16:48 +0900

    #15 message 사진 파일 추가

--- src/component/Message.js
+++ src/component/Message.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "../style/Message.module.css";
 import InnerTitle from "./InnerTitle";
+import folder from "../img/folder.png";
 
 function Message() {
     return (
         <div className={styles.mainWrapper}>
             <InnerTitle />
-            <div className={styles.messageList}>
-                
+            <div className={styles.folderContainer}>
+                <img src={folder} className={styles.folder}></img>
             </div>
         </div>
     );
