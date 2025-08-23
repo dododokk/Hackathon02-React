a7ca14c6342e34e3ee5b501a24fb04dcdeaf1b33
d0d0d0d0k <kdoeun05@konkuk.ac.kr>
2025-08-24 00:49:46 +0900

    #16 message 구조 수정

--- src/component/Message.js
+++ src/component/Message.js
@@ -2,13 +2,18 @@ import React from "react";
 import styles from "../style/Message.module.css";
 import InnerTitle from "./InnerTitle";
 import folder from "../img/folder.png";
+import message from "../img/message.png";
 
 function Message() {
     return (
         <div className={styles.mainWrapper}>
             <InnerTitle />
-            <div className={styles.folderContainer}>
-                <img src={folder} className={styles.folder}></img>
+            <div className={styles.folderContainer} style={{ backgroundImage: `url(${folder})` }}>
+                <div className={styles.messageHeader}>
+                    <img src={message} className={styles.messageImg} />
+                    <h2 className={styles.messageTitle}>채팅창</h2>
+                </div>
+                <hr className={styles.headerHr}/>
             </div>
         </div>
     );
